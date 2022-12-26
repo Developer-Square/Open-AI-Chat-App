74168981c8e029b7d243875a86a41cb99163cdad
ryann254 <keddelyronjoz@gmail.com>
2022-12-26 07:41:50 +0300

    feat: add frontend UI and logic

---
+++ client/script.js
@@ -0,0 +1,78 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+const loader = (element) => {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300);
+};
+
+const typeText = (element, text) => {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+};
+
+const generateUniqueID = () => {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+  return `id-${timestamp}-${hexadecimalString}`;
+};
+
+const chatStrpe = (isAi, value, uniqueID) => {
+  return `
+    <div class="wrapper ${isAi && 'ai'}">
+      <div class="chat">
+        <div class="profile">
+          <img src="${isAi ? bot : user}" alt="profile">
+        </div>
+        <div class="message" id=${uniqueID}>${value}</div>
+      </div>
+    </div>
+    `;
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStrpe(false, data.get('prompt'));
+  form.reset();
+
+  // bot's chatstripe
+  const uniqueID = generateUniqueID();
+  chatContainer.innerHTML += chatStrpe(true, ' ', uniqueID);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueID);
+  loader(messageDiv);
+};
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    handleSubmit(e);
+  }
+});
