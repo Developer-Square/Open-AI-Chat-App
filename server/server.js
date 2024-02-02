8d2203164312e4914cca9c59d07626dab3dbb2dc
ryann254 <keddelyronjoz@gmail.com>
2024-02-02 18:35:47 +0300

    fix: Update openai endpoint to new version

--- server/server.js
+++ server/server.js
@@ -1,17 +1,13 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-const configuration = new Configuration({
-  apiKey: process.env.OPEN_AI_SECRET_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI_SECRET_KEY });
 
 const app = express();
 
@@ -29,9 +25,9 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `${prompt}`,
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: 'system', content: prompt }],
+      model: 'gpt-4',
       temperature: 0,
       max_tokens: 3000,
       top_p: 1,
@@ -39,7 +35,7 @@ app.post('/', async (req, res) => {
       presence_penalty: 0,
     });
 
-    res.status(200).send({ bot: response.data.choices[0].text });
+    res.status(200).send({ bot: completion.choices[0].message.content });
   } catch (error) {
     console.log(error);
     res.status(500).send({ error });
